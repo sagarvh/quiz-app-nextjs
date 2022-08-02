ca80fa3225662492b93ce7dde8e0369664c08c05
arinddas <adasgupta60@gmail.com>
2022-08-02 23:18:55 +0530

    Initial commit 4

--- database/questions2.js
+++ database/questions2.js
@@ -3,12 +3,10 @@ const questions2 = [
     questionText: "It is difficult to successfully tamper with transactions within a blockchain",
     
     options: [ 
-    "1. Insufficient number of nodes to successfully tamper with",
-    "2. The bitcoin is an encrypted currency",
-    "3. The blockchain is not duplicated",
-    "4. Each participating node has a copy of the authentic transaction",
+    "1. The blockchain is not duplicated",
+    "2. Each participating node has a copy of the authentic transaction",
     ],
-          answer: "4. Each participating node has a copy of the authentic transaction",
+          answer: "2. Each participating node has a copy of the authentic transaction",
     },
     { 
     questionText: "What does Block in Blockchain contain",
@@ -35,11 +33,10 @@ const questions2 = [
     
     options: [ 
     "1. Programs that are stored on Blockchain and gets executed when a business logic is met",
-    "2. Run only on Ethereum Blockchain protocol",
-    "3. Deployed on all Blockchain Nodes",
-    "4. Both 1 and 3 are correct",
+    "2. Deployed on all Blockchain Nodes",
+    "3. Both 1 and 2 are correct",
     ],
-          answer: "4. Both 1 and 3 are correct",
+          answer: "3. Both 1 and 2 are correct",
     },
     { 
       questionText: "What does a NFT look like",
