40baef6b749f59ea9353f6e8b16e41ff3f56c206
Arindam Dasgupta <arinddas@in.ibm.com>
2022-08-22 14:43:59 +0530

    Update StartCard.js

--- pages/components/StartCard.js
+++ pages/components/StartCard.js
@@ -5,7 +5,7 @@ const StartCard = ({handleState, handleTimerStart}) => {
     <div className="flex flex-col justify-center h-[90vh]">
       <div className="lg:w-6/12 w-10/12 mx-auto px-10 py-16 outline outline-slate-300 outline-2 rounded-3xl shadow-xl">
         <h1 className="font-black font-Inter mb-6 drop-shadow-sm">
-          Blockchain Quiz Challenge | Career Carnival Kolkata 3.0 
+          Blockchain Quiz Challenge | Career Carnival Chennai 3.0 
         </h1>
         <p className=" font-medium drop-shadow-sm mb-5">
           Try to answer to following Blockchain-related questions.
